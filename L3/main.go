2a415f762beb6be747f9f28ee69eb8b1b0e141cd
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2023-12-14 13:38:43 +1300

    Arrays and slices

--- L3/main.go
+++ L3/main.go
@@ -12,8 +12,29 @@ func main() {
 	var ages = [3]int{20, 30, 40}
 
 	names := [4]string{"Ryan", "Ryan1", "Ryan2", "Ryan3"}
+	names[1] = "RyanN"
 
 	fmt.Println(ages, len(ages))
 	fmt.Println(names, len(names))
 
+	//slices (use arrays under the hood)
+
+	var scores = []int{100, 50, 60}
+	scores[2] = 25
+	fmt.Println(scores)
+	scores = append(scores, 200)
+	fmt.Println(scores, len(scores))
+
+	//slice ranges, endIndex exclusive
+	rangeOne := names[1:3]
+	//omit end to go all the way to the end
+	rangeTwo := names[2:]
+	//omit start vice versa, end Index exclusive
+	rangeThree := names[:3]
+	fmt.Println(rangeOne)
+	fmt.Println(rangeTwo)
+	fmt.Println(rangeThree)
+
+	rangeOne = append(rangeOne, "Kappa")
+	fmt.Println(rangeOne)
 }
