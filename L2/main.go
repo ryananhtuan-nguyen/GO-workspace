3a44885ba0a6cff385235c20ccf52efbe9309771
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2023-12-14 13:07:41 +1300

    print and println

---
+++ L2/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// Print, same line
+	fmt.Print("Hello, ")
+	fmt.Print("World! \n")
+	fmt.Print("New Line! \n")
+
+	fmt.Println("Another new line")
+	fmt.Println("Bye !")
+
+}
