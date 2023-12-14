667ee29258df94953bc98e90b393ced367eb2cc5
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2023-12-14 13:09:50 +1300

    print variables

--- L2/main.go
+++ L2/main.go
@@ -4,12 +4,16 @@ import "fmt"
 
 func main() {
 
+	age := 15
+	name := "Ryan"
+
 	// Print, same line
-	fmt.Print("Hello, ")
-	fmt.Print("World! \n")
-	fmt.Print("New Line! \n")
+	// fmt.Print("Hello, ")
+	// fmt.Print("World! \n")
+	// fmt.Print("New Line! \n")
 
-	fmt.Println("Another new line")
-	fmt.Println("Bye !")
+	// fmt.Println("Another new line")
+	// fmt.Println("Bye !")
 
+	fmt.Println("My age is", age, "and my name is", name)
 }
