ad86ae0616aafd5ff7f47887c6fe50b581c8ba5d
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2023-12-15 15:21:32 +1300

    continue statement

--- L6/main.go
+++ L6/main.go
@@ -9,4 +9,25 @@ func main() {
 	fmt.Println(age >= 50)
 	fmt.Println(age == 45)
 	fmt.Println(age != 50)
+
+	if age < 30 {
+		fmt.Println("Age is less than 30")
+	} else if age < 40 {
+		fmt.Println("Age is greater than 30 and less than 40")
+	} else {
+		fmt.Println("Age is not less than 40")
+	}
+
+	names := []string{"mario", "luigi", "yoshi", "peach", "bowser"}
+
+	for index, value := range names {
+		if index == 1 {
+			fmt.Println("Continuing at pos", index)
+			continue
+		}
+
+		fmt.Printf("The value at pos %v is %v \n", index, value)
+
+	}
+
 }
