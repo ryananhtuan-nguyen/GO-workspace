0ad510c2c65236d1faa0d3938e5f48fae8dba85f
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2023-12-15 15:44:02 +1300

    simple functions

--- L7/main.go
+++ L7/main.go
@@ -1,5 +1,19 @@
 package main
 
+import "fmt"
+
+func sayGreeting(n string) {
+	fmt.Printf("Greetings, %v\n", n)
+}
+
+func sayBye(n string) {
+	fmt.Printf("Bye, %v\n", n)
+}
 func main() {
+
+	sayGreeting("Mario")
+	sayGreeting("Luigi")
+
+	sayBye("Mario")
 
 }
