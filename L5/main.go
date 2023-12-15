acc2fb9cadd3882c79ea6c868e2be2ee6f3dcbe2
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2023-12-15 15:06:21 +1300

    Traditional for loop

---
+++ L5/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	//FOR LOOP
+
+	// x := 0
+	// for x < 5 {
+	// 	fmt.Println("value of x is:", x)
+	// 	x++
+	// }
+
+	//TRADITIONAL FOR LOOP
+	// for i := 0; i < 5; i++ {
+	// 	fmt.Println("value of i is:", i)
+
+	// }
+
+	//LOOPING THROUGH SLICES
+	name := []string{"mario", "moria", "yoda", "watermelon"}
+	fmt.Println(name)
+}
