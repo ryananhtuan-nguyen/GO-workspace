754679c14e25640ed864ac324ddee71525c1bb82
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2023-12-17 18:33:56 +1300

    receiver func

--- L13/bill.go
+++ L13/bill.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type bill struct {
 	name  string
 	items map[string]float64
@@ -11,9 +13,26 @@ type bill struct {
 func newBill(name string) bill {
 	b := bill{
 		name:  name,
-		items: map[string]float64{},
+		items: map[string]float64{"pie": 5.99, "cake": 3.99},
 		tip:   0,
 	}
 	return b
 
+}
+
+//format the bill
+
+func (b bill) format() string {
+	fs := "Bill breakdown: \n"
+	var total float64 = 0
+
+	//list items
+	for k, v := range b.items {
+		fs += fmt.Sprintf("%-25v ...$%v \n", k+":", v)
+		total += v
+	}
+	//total
+	fs += fmt.Sprintf("%-25v ...$%0.2f", "Total:", total)
+
+	return fs
 }
