e76f6e6648285003b0e4c4348936b4762ab3d61c
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2023-12-17 18:35:08 +1300

    init new lession

---
+++ L14/bill.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+type bill struct {
+	name  string
+	items map[string]float64
+	tip   float64
+}
+
+//make new bills
+
+func newBill(name string) bill {
+	b := bill{
+		name:  name,
+		items: map[string]float64{"pie": 5.99, "cake": 3.99},
+		tip:   0,
+	}
+	return b
+
+}
+
+//format the bill
+
+func (b bill) format() string {
+	fs := "Bill breakdown: \n"
+	var total float64 = 0
+
+	//list items
+	for k, v := range b.items {
+		fs += fmt.Sprintf("%-25v ...$%v \n", k+":", v)
+		total += v
+	}
+	//total
+	fs += fmt.Sprintf("%-25v ...$%0.2f", "Total:", total)
+
+	return fs
+}
