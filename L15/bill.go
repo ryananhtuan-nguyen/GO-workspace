14ea1fc8480e8e51ba7216c7d98be40460acc6a3
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2023-12-17 19:30:23 +1300

    init func

--- L15/bill.go
+++ L15/bill.go
@@ -13,7 +13,7 @@ type bill struct {
 func newBill(name string) bill {
 	b := bill{
 		name:  name,
-		items: map[string]float64{"pie": 5.99, "cake": 3.99},
+		items: map[string]float64{},
 		tip:   0,
 	}
 	return b
