85c30c9d4582103c97bf8512754446a27e58b387
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2023-12-17 19:54:50 +1300

    saved bill to file

--- L15/bill.go
+++ L15/bill.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 type bill struct {
 	name  string
@@ -49,4 +52,15 @@ func (b *bill) updateTip(tip float64) {
 
 func (b *bill) addItem(name string, price float64) {
 	b.items[name] = price
+}
+
+// save bill
+func (b *bill) save() {
+	data := []byte(b.format())
+
+	err := os.WriteFile("bills/"+b.name+".txt", data, 0644)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Bill saved to file")
 }
