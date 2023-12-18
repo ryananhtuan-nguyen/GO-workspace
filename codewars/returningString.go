d03d6e188762f8cd5efb55f372d0485cdea064f1
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2023-12-18 18:25:38 +1300

    Returning Strings

---
+++ codewars/returningString.go
@@ -0,0 +1,9 @@
+// Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+// [Make sure you type the exact thing I wrote or the program may not execute properly]
+package kata
+
+func Greet(name string) string {
+	// fill in solution here
+	return "Hello, " + name + " how are you doing today?"
+}
