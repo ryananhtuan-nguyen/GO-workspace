afdffef8a547ba854e54f5d33226c80107c11a74
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2023-12-19 16:26:56 +1300

    function to fetch CSV file and reformat

--- codewars/test.go
+++ codewars/test.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"encoding/csv"
 	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
 // Custom struct for CSV record
@@ -44,4 +47,87 @@ func getApiLinks(s string) ([]string, error) {
 	}
 
 	return links, nil
+}
+
+func fetchData() ([]MyData, []string, error) {
+	linksApiEndpoint := "https://api.onizmx.com/lambda/tower_stream"
+
+	links, err := getApiLinks(linksApiEndpoint)
+	if err != nil {
+		fmt.Println("Error fetching links:", err)
+		return nil, nil, err
+	}
+	var failedLinks []string
+
+	var allData []MyData
+
+	// Make a GET request to each API link
+	for _, link := range links {
+		response, err := http.Get(link)
+		if err != nil {
+			fmt.Println("Error making the request:", err)
+			failedLinks = append(failedLinks, link)
+			continue
+		}
+		defer response.Body.Close()
+
+		if response.StatusCode != http.StatusOK {
+			fmt.Printf("Error fetching CSV from %s: Status code %d\n", link, response.StatusCode)
+			failedLinks = append(failedLinks, link)
+			response.Body.Close()
+			continue
+		}
+
+		// Create a CSV reader
+		csvReader := csv.NewReader(response.Body)
+		csvReader.LazyQuotes = true
+
+		// Skip the header row
+		_, err = csvReader.Read()
+		if err != nil {
+			fmt.Println("Error reading CSV header:", err)
+			continue
+		}
+
+		// Initialize a slice to store parsed data
+		var data []MyData
+
+		// Read the rest of the CSV records
+		for {
+			record, err := csvReader.Read()
+			if err == io.EOF {
+				break
+			} else if err != nil {
+				fmt.Println("Error reading CSV:", err)
+				continue
+			}
+
+			// Ensure the record has at least 3 columns
+			if len(record) < 3 {
+				fmt.Println("Invalid CSV record:", record)
+				continue
+			}
+
+			// Parse CSV record and create MyData object
+			myData := MyData{
+				FarmID:  record[0],
+				TowerID: record[1],
+			}
+
+			// Parse RSSI field as float64
+			rssi, err := strconv.ParseFloat(strings.TrimSpace(record[2]), 64)
+			if err != nil {
+				fmt.Printf("Error parsing RSSI for record %v: %v\n", record, err)
+				continue // Skip this record if RSSI parsing fails
+			}
+			myData.RSSI = rssi
+
+			// Append MyData object to the slice
+			data = append(data, myData)
+		}
+
+		// Access data as needed, for example:
+		allData = append(allData, data...)
+	}
+	return allData, failedLinks, nil
 }
