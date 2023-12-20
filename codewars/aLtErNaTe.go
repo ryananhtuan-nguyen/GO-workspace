13c2b12221b93b92238cbf11fbaf599f40214808
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2023-12-20 19:54:59 +1300

    upper & lower case

---
+++ codewars/aLtErNaTe.go
@@ -0,0 +1,31 @@
+/*
+altERnaTIng cAsE <=> ALTerNAtiNG CaSe
+Define String.prototype.toAlternatingCase (or a similar function/method such as to_alternating_case/toAlternatingCase/ToAlternatingCase in your selected language; see the initial solution for details) such that each lowercase letter becomes uppercase and each uppercase letter becomes lowercase. For example:
+
+ToAlternatingCase("hello world"); // => "HELLO WORLD"
+ToAlternatingCase("HELLO WORLD"); // => "hello world"
+ToAlternatingCase("hello WORLD"); // => "HELLO world"
+ToAlternatingCase("HeLLo WoRLD"); // => "hEllO wOrld"
+ToAlternativeCase("12345"); // => "12345" (Non-alphabetical characters are unaffected)
+ToAlternativeCase("1a2b3c4d5e"); // => "1A2B3C4D5E"
+ToAlternativeCase("String.prototype.toAlternatingCase"); // => "sTRING.PROTOTYPE.TOaLTERNATINGcASE"
+As usual, your function/method should be pure, i.e. it should not mutate the original string.
+*/
+
+package kata
+
+import "strings"
+
+func ToAlternatingCase(str string) string {
+	var result = ""
+	strArr := strings.Split(str, "")
+	for _, v := range strArr {
+		if v == strings.ToUpper(v) {
+			result += strings.ToLower(v)
+		} else {
+			result += strings.ToUpper(v)
+		}
+	}
+
+	return result
+}
