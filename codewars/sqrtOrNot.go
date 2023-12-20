86a42fb943ac2e6c78c59a3f90ead2a9614ed8b5
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2023-12-20 19:36:54 +1300

    squareRoot

---
+++ codewars/sqrtOrNot.go
@@ -0,0 +1,30 @@
+// Write a method, that will get an integer array as parameter and will process every number from this array.
+
+// Return a new array with processing every number of the input-array like this:
+
+// If the number has an integer square root, take this, otherwise square the number.
+
+// Example
+// [4,3,9,7,2,1] -> [2,9,3,49,4,1]
+// Notes
+// The input array will always contain only positive numbers, and will never be empty or null.
+
+package kata
+
+import "math"
+
+func SquareOrSquareRoot(arr []int) []int {
+	results := make([]int, len(arr))
+
+	for i, x := range arr {
+		sqrt := math.Sqrt(float64(x))
+
+		if sqrt == math.Floor(sqrt) {
+			results[i] = int(sqrt)
+		} else {
+			results[i] = x * x
+		}
+	}
+
+	return results
+}
