4b00ff328b2c15bd6f897b23c85587efdb803d39
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2023-12-20 19:41:10 +1300

    return even or odd

---
+++ codewars/evenOrOdd.go
@@ -0,0 +1,10 @@
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+package kata
+
+func EvenOrOdd(number int) string {
+	if number%2 == 0 {
+		return "Even"
+	}
+	return "Odd"
+}
