4054e744f922851490a3c506749d43d6500cea21
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2023-12-21 15:52:19 +1300

    const rules

---
+++ L17-specialType/constants.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//Constants are declared like variables, but with keyword const
+	//can be character, string, bool, or numeric
+	//can not be declared using :=
+	const PI = 3.14159
+
+	fmt.Println(PI)
+}
