4983ad18b796929d0d24441b763527f3a177c773
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2023-12-22 20:18:48 +1300

    find all int in between

---
+++ codewars/isBetween.go
@@ -0,0 +1,17 @@
+// Complete the function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.
+
+// For example:
+
+// a = 1
+// b = 4
+// --> [1, 2, 3, 4]
+
+package kata
+
+func Between(a, b int) []int {
+	var result []int
+	for i := a; i <= b; i++ {
+		result = append(result, i)
+	}
+	return result
+}
