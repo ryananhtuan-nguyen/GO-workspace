9009371ba6dd34b3fe3e642f13a02bc9c76394f9
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2023-12-22 17:51:58 +1300

    count pos sum neg

---
+++ codewars/countPosSumNeg.go
@@ -0,0 +1,22 @@
+//Given an array of integers.
+
+// Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+// If the input is an empty array or is null, return an empty array.
+
+// Example
+// For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+
+package kata
+
+func CountPositivesSumNegatives(numbers []int) []int {
+	res := []int{0, 0}
+	for _, v := range numbers {
+		if v > 0 {
+			res[0] += 1
+		} else {
+			res[1] += v
+		}
+	}
+	return res
+}
