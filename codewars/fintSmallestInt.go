c12c78964529dcb5bc93ec836bc784481d420d3c
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2023-12-22 17:35:34 +1300

    find smallest item

---
+++ codewars/fintSmallestInt.go
@@ -0,0 +1,16 @@
+// Given an array of integers your solution should find the smallest integer.
+
+// For example:
+
+// Given [34, 15, 88, 2] your solution will return 2
+// Given [34, -345, -1, 100] your solution will return -345
+// You can assume, for the purpose of this kata, that the supplied array will not be empty.
+
+package kata
+
+import "sort"
+
+func SmallestIntegerFinder(numbers []int) int {
+	sort.Ints(numbers)
+	return numbers[0]
+}
