6d6508a10c8536a4ee369d621a4489f7b239b43c
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2023-12-24 17:05:42 +1300

    switch multiple cases

---
+++ codewars/quarterOfYear.go
@@ -0,0 +1,22 @@
+// Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
+
+// For example: month 2 (February), is part of the first quarter; month 6 (June), is part of the second quarter; and month 11 (November), is part of the fourth quarter.
+
+// Constraint:
+
+// 1 <= month <= 12
+
+package kata
+
+func QuarterOf(month int) int {
+	switch month {
+	case 1, 2, 3:
+		return 1
+	case 4, 5, 6:
+		return 2
+	case 7, 8, 9:
+		return 3
+	default:
+		return 4
+	}
+}
