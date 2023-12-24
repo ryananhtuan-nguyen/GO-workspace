a1e9581856f004f9626252fa3cb8408b5f8009b1
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2023-12-24 17:02:47 +1300

    str upper case

---
+++ codewars/makeUpperCase.go
@@ -0,0 +1,11 @@
+// Write a function which converts the input string to uppercase.
+
+package kata
+
+import (
+	"strings"
+)
+
+func MakeUpperCase(str string) string {
+	return strings.ToUpper(str)
+}
