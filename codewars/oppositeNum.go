d4b33d0fe17028ea95ff15a30897f2c7258a06dc
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2023-12-26 14:21:54 +1300

    opposite number

---
+++ codewars/oppositeNum.go
@@ -0,0 +1,13 @@
+// Very simple, given an integer or a floating-point number, find its opposite.
+
+// Examples:
+
+// 1: -1
+// 14: -14
+// -34: 34
+
+package kata
+
+func Opposite(value int) int {
+	return -value
+}
