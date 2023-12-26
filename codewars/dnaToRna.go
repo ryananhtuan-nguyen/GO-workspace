e0cae7e1f4f18cfd6a6e6ef7b6e394d050fdccd0
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2023-12-26 14:49:49 +1300

    string replaceAll

---
+++ codewars/dnaToRna.go
@@ -0,0 +1,18 @@
+// Deoxyribonucleic acid, DNA is the primary information storage molecule in biological systems. It is composed of four nucleic acid bases Guanine ('G'), Cytosine ('C'), Adenine ('A'), and Thymine ('T').
+
+// Ribonucleic acid, RNA, is the primary messenger molecule in cells. RNA differs slightly from DNA its chemical structure and contains no Thymine. In RNA Thymine is replaced by another nucleic acid Uracil ('U').
+
+// Create a function which translates a given DNA string into RNA.
+
+// For example:
+
+// "GCAT"  =>  "GCAU"
+// The input string can be of arbitrary length - in particular, it may be empty. All input is guaranteed to be valid, i.e. each input string will only ever consist of 'G', 'C', 'A' and/or 'T'.
+
+package kata
+
+import "strings"
+
+func DNAtoRNA(dna string) string {
+	return strings.ReplaceAll(dna, "T", "U")
+}
